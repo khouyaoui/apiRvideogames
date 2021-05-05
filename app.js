07e8914c2e6e44fd3db8a0753fe3d0db58fb597f
moxis <asif.ighir@gmail.com>
2021-05-05 18:15:34 +0200

    k dis kolci 2-1

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const morgan = require('morgan');
 const express = require("express");
 const app = express();
-// digha r da
 const usersRoute = require("./routes/users.route");
 const videogameRoute = require("./routes/videogames.route");
 const scoreRoute = require("./routes/score.route");
@@ -11,23 +10,22 @@ app.use(express.static("public"));
 
 const port = process.env.PORT || 4000;
 
-// app.use(express.json())
-// app.use(express.static("static"))
+
 app.use(express.json());
 app.use("/api/users", usersRoute);
 app.use("/api/videogames", videogameRoute);
 app.use("/api/scores", scoreRoute);
 
 app.all("/api/*", (req, res, next) => {
-  console.log(req.url);
-  next({
-    error: "Not found",
-  });
+    console.log(req.url);
+    next({
+        error: "Not found",
+    });
 });
 
 app.use((err, req, res, next) => {
-   res.json(err);
+    res.json(err);
 });
 
 app.listen(port, () => {
- });
+});
